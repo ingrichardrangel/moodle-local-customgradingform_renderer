06900fb61ee5afdfa3e073753228b1760fb7f118
Richard Rangel <32310953+ingrichardrangel@users.noreply.github.com>
2025-05-19 16:21:45 -0400

    Add files via upload

--- db/access.php
+++ db/access.php
@@ -1,1 +1,13 @@
+<?php
 
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = [
+    'local/customgradingform_renderer:import' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+            'manager' => CAP_ALLOW,
+        ],
+    ],
+];
