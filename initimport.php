1e50a58234e4eb71739e7dfedb8141fdee303302
Richard Rangel <32310953+ingrichardrangel@users.noreply.github.com>
2025-05-29 00:28:53 -0400

    Add files via upload

--- initimport.php
+++ initimport.php
@@ -1,25 +1,43 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Initializes grading area and redirects to import if rubric doesn't exist yet.
+ *
+ * @package   local_customgradingform_renderer
+ * @copyright 2025 Richard Rangel
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 require('../../config.php');
 require_once($CFG->dirroot . '/grade/grading/lib.php');
 
-// Required parameters
-$cmid = required_param('cmid', PARAM_INT);
-$debug = optional_param('debug', 0, PARAM_BOOL); // Set ?debug=1 in the URL to enable debug info
+$cmid  = required_param('cmid', PARAM_INT);
+$debug = optional_param('debug', 0, PARAM_BOOL);
 
-// Get course module
-$cm = get_coursemodule_from_id(null, $cmid, 0, false, MUST_EXIST);
+$cm      = get_coursemodule_from_id(null, $cmid, 0, false, MUST_EXIST);
 $context = context_module::instance($cm->id);
-$course = $DB->get_record('course', ['id' => $cm->course], '*', MUST_EXIST);
+$course  = $DB->get_record('course', ['id' => $cm->course], '*', MUST_EXIST);
 
-// User authentication and capability check
 require_login($course, true, $cm);
 require_capability('moodle/grade:managegradingforms', $context);
 
-// Determine grading component and area
 $component = 'mod_' . $cm->modname;
-$area = 'submissions'; // Default for assign module
+$area = 'submissions';
 
-// Fetch or create grading area
 $gradingarea = $DB->get_record('grading_areas', [
     'contextid' => $context->id,
     'component' => $component,
@@ -38,7 +56,6 @@ if (!$gradingarea) {
     $DB->update_record('grading_areas', $gradingarea);
 }
 
-// Check if a rubric is already defined
 $alreadyexists = $DB->record_exists('grading_definitions', [
     'areaid' => $gradingarea->id,
     'method' => 'rubric'
@@ -47,25 +64,26 @@ $alreadyexists = $DB->record_exists('grading_definitions', [
 $returnurl = new moodle_url('/mod/' . $cm->modname . '/view.php', ['id' => $cm->id]);
 
 if ($alreadyexists) {
+    $message = get_string('rubricalreadydefined', 'local_customgradingform_renderer');
+
     if ($debug) {
         echo $OUTPUT->header();
-        echo $OUTPUT->notification('⚠️ A rubric is already defined for this activity. Importing a new one is not allowed.', 'notifyproblem');
-        echo html_writer::start_div('debug', ['style' => 'border-left:4px solid red;padding:10px;margin:10px 0']);
-        echo "<strong>cmid:</strong> {$cmid}<br>";
-        echo "<strong>contextid:</strong> {$context->id}<br>";
-        echo "<strong>component:</strong> {$component}<br>";
-        echo "<strong>area:</strong> {$area}<br>";
-        echo "<strong>gradingarea id:</strong> {$gradingarea->id}<br>";
+        echo $OUTPUT->notification($message, 'notifyproblem');
+        echo html_writer::start_div('debug');
+        echo html_writer::tag('p', "cmid: {$cmid}");
+        echo html_writer::tag('p', "contextid: {$context->id}");
+        echo html_writer::tag('p', "component: {$component}");
+        echo html_writer::tag('p', "area: {$area}");
+        echo html_writer::tag('p', "gradingarea id: {$gradingarea->id}");
         echo html_writer::end_div();
         echo $OUTPUT->continue_button($returnurl);
         echo $OUTPUT->footer();
         exit;
     } else {
-        redirect($returnurl, '⚠️ A rubric is already defined for this activity.', 5);
+        redirect($returnurl, $message, 5);
     }
 }
 
-// Redirect to import script
 $importurl = new moodle_url('/local/customgradingform_renderer/import.php', [
     'areaid' => $gradingarea->id,
     'contextid' => $context->id,
