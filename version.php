d9475d6350ec91715ff310ff177880923541fd7e
Richard Rangel <32310953+ingrichardrangel@users.noreply.github.com>
2025-06-08 00:07:06 -0400

    Update version.php
    Matching Minimum requirement to be the same has moodle-block_importrubric
    
    Updating version from 1.0.2 to 1.0.3

--- version.php
+++ version.php
@@ -26,8 +26,8 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_customgradingform_renderer'; // Full name of the plugin (used for diagnostics).
 $plugin->version   = 2025053000; // Plugin version (YYYYMMDDXX).
-$plugin->requires  = 2022111500; // Minimum required Moodle version (Moodle 4.1).
+$plugin->requires  = 2022041900; // Minimum required Moodle version (Moodle 4.1).
 $plugin->maturity = MATURITY_STABLE; // Development maturity level.
-$plugin->release = '1.0.2'; // Human-readable release name.
+$plugin->release = '1.0.3'; // Human-readable release name.
 
 $plugin->dependencies = []; // No other plugin dependencies.
